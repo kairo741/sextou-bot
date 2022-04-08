0b6f864978c0ea4a830f3ed99a0229d1d243086d
Kairo <kairo.amorim2001@gmail.com>
2022-04-08 16:13:39 -0300

    Adicionada info sobre o author do bot e o Dj da Música

--- constants.py
+++ constants.py
@@ -21,6 +21,20 @@ SEXTA_DOS_CRIAS_LYRICS = """
     NA ???? DO PAIIII
 """
 
+DJ_RAMON_SUCESSO_DESC = """🐦 @djramonsucesso
+☎ CONTRATE JÁ: (21) 98024-6523/ (21)98251-1319
+▶ https://www.youtube.com/channel/UCozKPD9B3h4opITRE1Wd-Ow
+☁ https://soundcloud.com/ramon-sucesso22
+📷 https://www.instagram.com/djramonsucessofc/
+🎶 https://open.spotify.com/artist/4TU1r1V7Xp6Ov2X2irfm3J
+🛒 https://music.amazon.com.br/artists/B08P747KY2/dj-ramon-sucesso
+⚫ https://tidal.com/browse/artist/22372654
+♪ https://www.tiktok.com/@djramonsucessoofc"""
+
+ME_DESC = """🐦 @KairoAmorim
+📧 kairo.amorim2001@gmail.com
+🐙 https://github.com/kairo741"""
+
 SEXTA_DOS_CRIAS_MP4 = "files/sexta_dos_crias.mp4"
 SEXTA_DOS_CRIAS_GIF = "files/sexta_dos_crias_gif.gif"
 SEXTA_DOS_CRIAS_MP4_COMPLETE_EDITION = "files/sexta_dos_crias_complete_edition.mp4"
--- main.py
+++ main.py
@@ -15,7 +15,7 @@ async def on_ready():
         activity=discord.Streaming(name="SEXTA DOS CRIAS",
                                    url="https://www.youtube.com/watch?v=6zF3H1vH-6g",
                                    platform="Sexta"))
-    print("Bot sextouu")
+    print(choice([ascii.SEXTOU_1, ascii.SEXTOU_2, ascii.SEXTOU_3, ascii.SEXTOU_4]))
 
 
 @client.command("sextou")
@@ -55,6 +55,34 @@ async def send_avatar(context):
     message.set_image(
         url="https://cdn.discordapp.com/avatars/954427348315418694/ea558ea1dc3ae7fbeba1476a0fe4eda2.png?size=2048")
     await context.send(embed=message)
+
+
+@client.command("author")
+async def send_to_info(context):
+    await send_author_info(context)
+
+
+@client.command("info")
+async def send_author_info(context):
+    message = discord.Embed(title="Infos",
+                            colour=discord.Colour.dark_red())
+
+    message.add_field(name="Author do Bot - Kairo Amorim",
+                      value=constants.ME_DESC,
+                      inline=False)
+    # To break a line
+    message.add_field(name=chr(173), value=chr(173))
+
+    message.add_field(name="DJ - DJ RAMON SUCESSO",
+                      value=constants.DJ_RAMON_SUCESSO_DESC,
+                      inline=False)
+
+    spotify_message = discord.Embed(title="“O cara do tambor bolha”💦",
+                               colour=discord.Colour.dark_red()).set_image(
+        url="https://yt3.ggpht.com/Y50i3LmM9Vqb2x_iI5sWnYLm7hYJ5nVk8nvktGZhuWNA5uFyXfqFyEN63ra7jbiPzVA4jxcY-ota=s640"
+            "-c-fcrop64=1,00000000ffffffff-nd-v1")
+    await context.send(embed=message)
+    await context.send(embed=spotify_message)
 
 
 client.run(os.environ["BOT_TOKEN"])
